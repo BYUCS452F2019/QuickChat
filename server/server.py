1c43578750112d3994c54b97ee8474ca504b6ace
janders111 <jordananders1@gmail.com>
2019-10-26 14:46:26 -0600

    readme and bug fix in server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,42 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__, static_url_path='', static_folder='../view/dist')
 
-#GET /login?username=  returns chatrooms array
-#GET /addchatroom?chatroom=   returns ok/error
-#POST /sendchat?chatroom=x, message in json  returns ok/error
-#GET /getmessages?chatroom=  returns messages array
+#####################################Static File Serving
+@app.route('/')
+def root():
+        return app.send_static_file('index.html')
+
+@app.route('/index.html')
+def index():
+        return root()
+
+@app.route('/index-webpack.js')
+def indexwebpack():
+        return app.send_static_file('index-webpack.js')
+
+
+#####################################Other get/post functions
+#example: http://127.0.0.1:5000/login?username=asdf
+@app.route('/login')
+def login():
+        username = request.args.get('username')
+        return 'got username ' + username
+
+@app.route('/addchatroom')
+def addchatroom():
+        chatroom = request.args.get('chatroom')
+        return 'got arg ' + chatroom
+
+@app.route('/sendchat')
+#example: http://127.0.0.1:5000/sendchat?chatroom=chatpoo&username=jordan
+def sendchat():
+        chatroom = request.args.get('chatroom')
+        username = request.args.get('username')
+        return 'got arg ' + username + ' and ' + chatroom
+
+@app.route('/getmessages')
+def getmessages():
+        chatroom = request.args.get('chatroom')
+        return 'got arg ' + chatroom
+
